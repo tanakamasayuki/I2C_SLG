ba9887edf95b79d2e2e187f4ff8b4695bd39136e
root <akiraneko@gmail.com>
2021-03-02 17:26:22 +0900

    iAdd WebSLG examples

---
+++ examples/WebSLG/WebSLG.ino
@@ -0,0 +1,208 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include "html.h"
+#include "I2C_SLG46826.h"
+
+WebServer server(80);
+
+#define SLG46826_I2C_SDA 32
+#define SLG46826_I2C_SCL 33
+
+I2C_SLG46826 slg(Wire);
+
+void handleRoot(void) {
+  String html = htmlBodyString;
+  html.replace("#TITLE#", "SLG Lib Home");
+  String body = "";
+
+  for (int controlCode = 0; controlCode < 16; controlCode++) {
+    String level[2] = {"LOW", "HIGH"};
+    if (slg.scanSlg(controlCode)) {
+      // GPIO
+      String htmlItem = htmlIODisplayString;
+      htmlItem.replace("#CONTROL_CODE#", String(controlCode));
+      htmlItem.replace("#IO0#", level[slg.digitalRead(controlCode, 0)]);
+      htmlItem.replace("#IO1#", level[slg.digitalRead(controlCode, 1)]);
+      htmlItem.replace("#IO2#", level[slg.digitalRead(controlCode, 2)]);
+      htmlItem.replace("#IO3#", level[slg.digitalRead(controlCode, 3)]);
+      htmlItem.replace("#IO4#", level[slg.digitalRead(controlCode, 4)]);
+      htmlItem.replace("#IO5#", level[slg.digitalRead(controlCode, 5)]);
+      htmlItem.replace("#IO9#", level[slg.digitalRead(controlCode, 9)]);
+      htmlItem.replace("#IO10#", level[slg.digitalRead(controlCode, 10)]);
+      htmlItem.replace("#IO11#", level[slg.digitalRead(controlCode, 11)]);
+      htmlItem.replace("#IO12#", level[slg.digitalRead(controlCode, 12)]);
+      htmlItem.replace("#IO13#", level[slg.digitalRead(controlCode, 13)]);
+      htmlItem.replace("#IO14#", level[slg.digitalRead(controlCode, 14)]);
+
+      body += htmlItem;
+
+      // Read
+      uint8_t data[256];
+      htmlItem = "<h3>Read</h3>\n";
+      memset(data, 0, sizeof(data));
+      if (slg.readSlg(controlCode, data)) {
+        for (int i = 0; i < 16; i++) {
+          for (int j = 0; j < 16; j++) {
+            char num[16];
+            snprintf(num, 16, "%02X", data[i * 16 + j]);
+            htmlItem += num;
+          }
+          htmlItem += "<br />\n";
+        }
+      }
+      body += htmlItem;
+
+      // Erase
+      htmlItem = "<h3>Erase</h3>\n";
+      htmlItem += "[<a href=\"/erase?controlCode=" + String(controlCode) + "\">Erase</a>]<br />\n";
+      body += htmlItem;
+
+      // Write
+      htmlItem = htmlWriteString;
+      htmlItem.replace("#CONTROL_CODE#", String(controlCode));
+      body += htmlItem;
+    }
+  }
+
+  html.replace("#BODY#", body);
+
+  server.send(200, "text/html", html);
+}
+
+void handleErase(void) {
+  int controlCode = server.arg("controlCode").toInt();
+
+  String html = htmlBodyString;
+  html.replace("#TITLE#", "SLG Lib Erase");
+
+  String body = "";
+  body += "<h2>Erase</h2>\n";
+  if (slg.eraseSlg(controlCode)) {
+    body += "SLG46826 erased.<br />\n";
+  } else {
+    body += "SLG46826 erase failure!<br />\n";
+  }
+
+  body += "Resetting SLG46826.<br />\n";
+  slg.resetSlg(controlCode);
+
+  body += "<br />\n";
+  body += "[ <a href=\"/\">Retarn HOME</a> ]<br />\n";
+
+  html.replace("#BODY#", body);
+  server.send(200, "text/html", html);
+}
+
+void handleWrite(void) {
+Serial.println("handleWrite");
+  int controlCode = server.arg("controlCode").toInt();
+  String hexString = server.arg("data");
+  String html = htmlBodyString;
+  html.replace("#TITLE#", "SLG Lib Write");
+
+  String body = "";
+  body += "<h2>Write</h2>\n";
+
+  uint8_t hexData[16][16];
+  memset(hexData, 0, sizeof(hexData));
+  uint16_t hexStrLen = hexString.length();
+
+  uint16_t pos = 0;
+  for (int i = 0; i < 16; i++) {
+    // find :
+    while (pos < hexStrLen && hexString[pos] != ':') {
+      pos++;
+    }
+
+    // data check
+    if (hexStrLen < (pos + 43)) {
+      body += "Intel HEX parse error!<br />\n";
+      body += "<br />\n";
+      body += "[ <a href=\"/\">Retarn HOME</a> ]<br />\n";
+
+      html.replace("#BODY#", body);
+      server.send(200, "text/html", html);
+      return;
+    }
+
+    // data skip
+    pos += 9;
+
+    // hex to data
+    for (int j = 0; j < 16; j++) {
+      uint8_t byteData = 0;
+      if ('A' <= hexString[pos]) {
+        byteData += (hexString[pos] - 'A' + 10) * 16;
+      } else {
+        byteData += (hexString[pos] - '0') * 16;
+      }
+      pos++;
+      if ('A' <= hexString[pos]) {
+        byteData += hexString[pos] - 'A' + 10;
+      } else {
+        byteData += hexString[pos] - '0';
+      }
+      pos++;
+      hexData[i][j] = byteData;
+    }
+  }
+
+  // print
+  for (int i = 0; i < 16; i++) {
+    for (int j = 0; j < 16; j++) {
+      char num[16];
+      snprintf(num, 16, "%02X", hexData[i][j]);
+      body += num;
+    }
+    body += "<br />\n";
+  }
+  body += "<br />\n";
+
+  slg.eraseSlg(controlCode);
+  slg.resetSlg(controlCode);
+
+  if (slg.writeSlg(0, &hexData[0][0])) {
+    body += "Write SLG46826.<br />\n";
+  } else {
+    body += "Write SLG46826 failure!<br />\n";
+  }
+  slg.resetSlg(0);
+  body += "Resetting SLG46826.<br />\n";
+
+  body += "<br />\n";
+  body += "[ <a href=\"/\">Retarn HOME</a> ]<br />\n";
+
+  html.replace("#BODY#", body);
+  server.send(200, "text/html", html);
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(500);
+
+  // Init I2C
+  Wire.begin(SLG46826_I2C_SDA, SLG46826_I2C_SCL);
+
+  WiFi.begin();
+  Serial.println("WiFi ");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+
+  Serial.println("WiFi Connected.");
+  Serial.print("IP = ");
+  Serial.println(WiFi.localIP());
+
+  // bind
+  server.on("/", handleRoot);
+  server.on("/erase", handleErase);
+  server.on("/write", handleWrite);
+
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();
+}
---
+++ examples/WebSLG/html.h
@@ -0,0 +1,46 @@
+const char* htmlBodyString = R"html(
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>#TITLE#</title>
+</head>
+<body>
+#BODY#
+</body>
+</html>
+)html";
+
+const char* htmlIODisplayString = R"html(
+<H2>Control Code = #CONTROL_CODE#</H2>
+<H3>GPIO</H3>
+<table border="1">
+<tr><th>IO</th><th>Level</th><th>VCC</th><th>Description</th></tr>
+<tr><td>0</td><td>#IO0#</td><td>VCC</td><td>GPIO</td></tr>
+<tr><td>1</td><td>#IO1#</td><td>VCC</td><td>GPIO/Vref IN</td></tr>
+<tr><td>2</td><td>#IO2#</td><td>VCC</td><td>GPIO/SLA_0</td></tr>
+<tr><td>3</td><td>#IO3#</td><td>VCC</td><td>GPIO/SLA_1</td></tr>
+<tr><td>4</td><td>#IO4#</td><td>VCC</td><td>GPIO/SLA_2</td></tr>
+<tr><td>5</td><td>#IO5#</td><td>VCC</td><td>GPIO/SLA_3</td></tr>
+<tr><td>6</td><td></td><td>VCC</td><td>GPO(Output ONLY)</td></tr>
+<tr><td>7</td><td></td><td>VCC2</td><td>GPO(Output ONLY)</td></tr>
+<tr><td>8</td><td></td><td>VCC2</td><td>GPO(Output ONLY)</td></tr>
+<tr><td>9</td><td>#IO9#</td><td>VCC2</td><td>GPIO/Vref_OUT1</td></tr>
+<tr><td>10</td><td>#IO10#</td><td>VCC2</td><td>GPIO/Vref_OUT0</td></tr>
+<tr><td>11</td><td>#IO11#</td><td>VCC2</td><td>GPIO/ACMP3L_IN</td></tr>
+<tr><td>12</td><td>#IO12#</td><td>VCC2</td><td>GPIO/ACMP2L_IN</td></tr>
+<tr><td>13</td><td>#IO13#</td><td>VCC2</td><td>GPIO/ACMP1H_IN</td></tr>
+<tr><td>14</td><td>#IO14#</td><td>VCC2</td><td>GPIO/ACMP0H_IN</td></tr>
+</table>
+)html";
+
+const char* htmlWriteString = R"html(
+<h3>Write</h3>
+<form action="write" method="get">
+  <input type="hidden" name="controlCode" value="#CONTROL_CODE#" />
+  <textarea name="data" rows="4" cols="60">
+
+</textarea><br />
+  <input type="submit">
+</form>
+)html";
